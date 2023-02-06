9cde867cdadebcf87378367abe639bac7fe5202b
katas8n <kirka6061512@icloud.com>
2023-02-06 21:12:22 +0200

    ж)

--- lesson-3__FALSY_data-types-switch-binary-unary/cycles.js
+++ lesson-3__FALSY_data-types-switch-binary-unary/cycles.js
@@ -26,3 +26,60 @@
 // else => WTH?
 
 // *
+
+// const MAGIC_NUMBER = 23;
+
+// let i = 0;
+
+// let isOn = true;
+
+// // while - пока , до тех пор
+// while (isOn) {
+//   let userChoose = +prompt("Enter your number : ");
+
+//   i++;
+//   console.log("[ATTEMPTS]", i);
+
+//   if (userChoose === MAGIC_NUMBER) {
+//     console.log("Amazing");
+
+//     isOn = false;
+//   } else if (userChoose < MAGIC_NUMBER) {
+//     console.log("ur number less than mine");
+//   } else if (userChoose > MAGIC_NUMBER) {
+//     console.log("ur number bigger than mine");
+//   } else {
+//     console.log("WTH?");
+//   }
+// }
+
+// while (false) {
+//   console.log("Here we are!!!");
+// }
+// let i = 0;
+
+// do ... while
+// do {
+//   console.log(i);
+//   i++;
+//   console.log("Here we are");
+// } while (i === 0);
+
+// for
+
+// break ;
+// continue;
+
+// for (let i = 0; i < 10; i++) {
+//   if (i === 5 || i === 7) {
+//     continue;
+//   }
+
+//   console.log(i);
+// }
+
+// Fuzz Biz
+
+// Если число нацело делится на 3 , тогда выводи слово "Фаз"
+// Если число нацело делится на 5 , тогда выводи слово "Бизз"
+// Если число нацело делится на 3 и на 5 , тогда выводи "ФазБизз"
