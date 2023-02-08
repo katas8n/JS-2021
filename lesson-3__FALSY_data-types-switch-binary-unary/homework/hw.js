27ddc7cdfb7d530dc3955b01c656d5dcfe4550a3
katas8n <kirka6061512@icloud.com>
2023-02-08 19:33:35 +0200

    TS

--- lesson-3__FALSY_data-types-switch-binary-unary/homework/hw.js
+++ lesson-3__FALSY_data-types-switch-binary-unary/homework/hw.js
@@ -1,5 +1,23 @@
 // 1) Via "for" console every odd number from 20
+
+// const number = 20;
+
+// for (let i = 0; i < number; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
 // 2) Replace "for" on "while"
+
+// let i = 0;
+
+// let number = 20;
+
+// while(i < number) {
+//     // if(i )
+// }
+
 // 3) Face-control
 // const age = prompt("How old are you ?");
 
@@ -11,3 +29,12 @@
 // 4) Output every simple number from 2 to 10;
 // 5) Get number from user and get factorial of this number
 // 5 = 1 * 2 * 3 * 4 * 5 = 125
+
+// let sum = 1;
+
+// for (let i = 5; i > 0; i = i - 1) {
+//   if (i === 0) continue;
+//   sum = i * sum;
+//   console.log(i);
+//   console.log(sum);
+// }
