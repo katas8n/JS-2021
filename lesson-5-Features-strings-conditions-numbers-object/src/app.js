ad2b03a2ac391f1c0efb0caba53de58f69e72e78
katas8n <kirka6061512@icloud.com>
2023-02-13 20:10:35 +0200

    task ;)

--- lesson-5-Features-strings-conditions-numbers-object/src/app.js
+++ lesson-5-Features-strings-conditions-numbers-object/src/app.js
@@ -39,3 +39,78 @@
 // const studentId: id = "id3200723";
 // const adminId: id = "id3200123";
 // const lectorId: id = "id3200223";
+// const number1: number = 23;
+// const number2: number = 13;
+// const equation: string = `23 + 13 = ${number1 + number2}`;
+// console.log(equation);
+// * WeekDay
+// 1) Get day of the week
+// 2) If Sunday : Today is beutiful day
+// else if Monday : Today is awful day
+// else if Tuesday : Today is thick day
+// else if Wednesday: Today is middle of the week
+// else if Thursday: Today is the day befor Friday
+// else if Friday: Today is super-pupperDay!!!
+// else if Saturday: Today is super-pupperDay!!!
+// else : incorrect data and return user to  1) Get day of the week
+// type WeekDay =
+//   | "sunday"
+//   | "monday"
+//   | "tuesday"
+//   | "wednesday"
+//   | "thursday"
+//   | "friday"
+//   | "saturday";
+// let switcher = true;
+// while (switcher) {
+//   const dayOfWeek: WeekDay = prompt("What day is it today? / Exit q)");
+//   switch (dayOfWeek.toLowerCase()) {
+//     case "sunday":
+//       console.log("Today is beutiful day");
+//       switcher = !confirm("Do you wanna exit ?");
+//       break;
+//     case "monday":
+//       console.log("Today is awful day");
+//       switcher = !confirm("Do you wanna exit ?");
+//       break;
+//     case "tuesday":
+//       console.log("Today is thick day");
+//       switcher = !confirm("Do you wanna exit ?");
+//       break;
+//     case "wednesday":
+//       console.log("Today is middle of the week");
+//       switcher = !confirm("Do you wanna exit ?");
+//       break;
+//     case "thursday":
+//       console.log("Today is middle of the week");
+//       switcher = !confirm("Do you wanna exit ?");
+//       break;
+//     case "friday":
+//       console.log("Today is middle of the week");
+//       switcher = !confirm("Do you wanna exit ?");
+//       break;
+//     case "saturday":
+//       console.log("Today is middle of the week");
+//       switcher = !confirm("Do you wanna exit ?");
+//       break;
+//     case "q":
+//       switcher = !confirm("Do you wanna exit ?");
+//       break;
+//     default:
+//       console.log("Something went wrong try again!");
+//       break;
+//   }
+// }
+// type yes = true;
+// type no = false;
+// * Task
+// 1) Greeting
+// 2) Get userName - userName must contain more than 2 symbols
+//                 - userName must be convert into upper case
+//                 - cut last 3 letters from name and reverse it
+// 3) Get user age => Age must be positive number
+//                 => Age must be a number
+//                 => If age is not a number -> return user to prompt("Enter your age")
+// 4) Узнать в год какого животного был рожден пользователь
+// 4.1) Get month of birth and узнать кто наш пользователь по знак зодиака
+// 5) Every step must keep propose to user => Do you wanna exit ?
