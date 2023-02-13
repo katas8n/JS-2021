4098879cf9c2006389075c27201f817ad6cd89bb
katas8n <kirka6061512@icloud.com>
2023-02-13 19:22:00 +0200

    Something changed

---
+++ lesson-5-Features-strings-conditions-numbers-object/app.ts
@@ -0,0 +1,15 @@
+// * number
+const num: number = 23;
+
+let result: number = num + 13;
+
+// console.log(result);
+
+// result = num / 13;
+// console.log(result);
+
+// result = num * 13;
+// console.log(result);
+
+// result = num % 13;
+// console.log(result);
--- lesson-5-Features-strings-conditions-numbers-object/src/app.js
+++ lesson-5-Features-strings-conditions-numbers-object/src/app.js
@@ -1,1 +1,11 @@
 "use strict";
+// * number
+const num = 23;
+let result = num + 13;
+// console.log(result);
+// result = num / 13;
+// console.log(result);
+// result = num * 13;
+// console.log(result);
+// result = num % 13;
+// console.log(result);
