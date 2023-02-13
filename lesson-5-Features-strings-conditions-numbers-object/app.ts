01dc7e0d1064249e01155132dbc9cf83cddec9d1
katas8n <kirka6061512@icloud.com>
2023-02-13 19:38:14 +0200

    something

--- lesson-5-Features-strings-conditions-numbers-object/app.ts
+++ lesson-5-Features-strings-conditions-numbers-object/app.ts
@@ -1,7 +1,12 @@
 // * number
-const num: number = 23;
+// let num:number = 23;
+// num = 13;
+// num = 11;
+// num = "HEllo world";
+// num = true;
+// const num: number = 23;
 
-let result: number = num + 13;
+// let result: number = num + 13;
 
 // console.log(result);
 
@@ -13,3 +18,45 @@ let result: number = num + 13;
 
 // result = num % 13;
 // console.log(result);
+
+// * string
+
+// const str: string = "Hello world";
+
+// const result: string = "Hello world" + true;
+// const result: string = "Hello world" + 23;
+
+// * boolean
+
+// let isAdmin: boolean = true;
+
+// let result: boolean = true + 2;
+
+// ! null , undefined
+
+// let kek: undefined = undefined;
+// let kek: null = null;
+// kek = 23;
+
+// * feature
+// * any - whatever !!!!11!
+// let num: string;
+
+// let num:string = "It's a string!";
+// num = 23;
+
+// 5 primitives and 1 special object
+// ! Type Alias
+
+// type names = string | null;
+
+// const firstName: names = "John";
+
+// ! Union
+// type id = "id3200723" | "id3200123";
+
+// const studentId: id = "id3200723";
+
+// const adminId: id = "id3200123";
+
+// const lectorId: id = "id3200223";
