3d70996ab3ebca1756ba42b52a37fea61578a87e
katas8n <kirka6061512@icloud.com>
2023-02-15 21:39:28 +0200

    Practice

---
+++ module-lvl-2/lesson-6-object-functions-array-methods/src/scripts/practice.js
@@ -0,0 +1,17 @@
+// TODO : looks as a function
+// TODO : name.length > 5
+// TODO : name contains "a"
+// TODO : name NOT an empty string / not a number / not null|undefined
+
+// TODO : surname NOT an empty string / not a number / not null|undefined
+// TODO : surname contains "a"
+// TODO : surname doesn't contain "- , _ , = , ! , ?"
+// TODO : surname.length > 3 && < 10
+
+// TODO : age === number
+// TODO : age NOT < 0 and NOT > 100
+// TODO : age is exist
+
+// 1. Greeting
+// 2. Register
+// 3. { name : ... , age : ... , surname : ...}
