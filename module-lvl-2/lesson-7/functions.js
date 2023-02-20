9f2986bde047536bb1977eb59636c13bf34771ba
katas8n <kirka6061512@icloud.com>
2023-02-20 22:09:11 +0200

    Homework

--- module-lvl-2/lesson-7/functions.js
+++ module-lvl-2/lesson-7/functions.js
@@ -3,3 +3,79 @@
 // }
 
 // console.log(howOldAreYou(1998));
+
+// const GLOBAL = "global vision";
+
+// // console.log("[ GLOBAL in GLOBAL VISION [9] ]", GLOBAL);
+
+// function getGlobal(localGlobalValue) {
+//   if (!localGlobalValue) localGlobalValue = "There is nothing here yet";
+
+//   const alertsValue = localGlobalValue + "!!!";
+
+//   console.log("[ GLOBAL in FUNCTION VISION [into_the_function] ]", alertsValue);
+
+//   return alertsValue;
+// }
+
+// console.log(getGlobal(GLOBAL));
+// console.log(getGlobal("There was mr Biden into the Aroma "));
+// getGlobal("We into the function body");
+// getGlobal(true);
+// getGlobal();
+
+// function toScreemCase(string = "There is nothing") {
+//   return (
+//     string.toUpperCase().slice(string.length / 2 - 2, string.length / 2 + 10) +
+//     "!!!"
+//   );
+// }
+
+// const upperCase = toScreemCase("admin , is in it-step always");
+// const emptyFunction = toScreemCase();
+// console.log("[upperCase]", upperCase);
+// console.log("[emptyFunction]", emptyFunction);
+
+// function result(arg) {
+//   return 1;
+// }
+
+// function muliplyOnFive(number) {
+//   return number * 5;
+// }
+
+// const kek = result(200000000);
+
+// const twenty = muliplyOnFive(4);
+// console.log(kek);
+// console.log(twenty);
+
+// let isRunning = true;
+
+// function getSymbol(symbol) {
+//   if (!symbol) return;
+
+//   const firstNumber = prompt("Enter your first number ?");
+//   const secondNumber = prompt("Enter your second number ?");
+
+//   const result = calculate(firstNumber, secondNumber, symbol);
+//   console.log("[result]", result);
+// }
+
+// function calculate(a, b, symbol) {
+//   return `${a} ${symbol} ${b}`;
+// }
+
+// function isExit(userAnswer) {
+//   if (userAnswer === "1") return false;
+
+//   return true;
+// }
+
+// while (isRunning) {
+//   isRunning = isExit(prompt("1) If u wanna leave 2) Next step"));
+
+//   getSymbol(prompt("1) + 2) - 3) * 4) /"));
+// }
+
+// 1) Calculator
